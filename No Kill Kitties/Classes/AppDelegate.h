f2abffa4ab43d7ed4c9a3e282849594f3e2deb87
Calvin Webster <calweb@gmail.com>
2012-01-22 14:59:39 -0500

    Initial Commit

---
+++ No Kill Kitties/Classes/AppDelegate.h
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.h
+//  No Kill Kitties
+//
+//  Created by Calvin Webster on 1/22/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#ifdef PHONEGAP_FRAMEWORK
+	#import <PhoneGap/PGAppDelegate.h>
+    #import <PhoneGap/PGViewController.h>
+#else
+	#import "PGAppDelegate.h"
+    #import "PGViewController.h"
+#endif
+
+@interface AppDelegate : PGAppDelegate < PGCommandDelegate > {
+
+	NSString* invokeString;
+}
+
+// invoke string is passed to your app on launch, this is only valid if you 
+// edit No Kill Kitties.plist to add a protocol
+// a simple tutorial can be found here : 
+// http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
+
+@property (copy)  NSString* invokeString;
+
+@end
+
